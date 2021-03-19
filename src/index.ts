0a5a8e289c7bc6405884e0cf2856af2192b08d3a
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-03-19 22:24:57 +0000

    add hello world example

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import "./config"; // imports are hoisted so this needs to come first
 import { app } from "./app";
 import { getEnv } from "./util/getEnv";
 
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  if (message.subtype === undefined) {
+    await say(`Hey there <@${message.user}>!`);
+  }
+});
+
 (async () => {
   // Start your app
   await app.start(parseInt(getEnv("PORT")));
