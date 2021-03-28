08a927befeb61ce6a8bc23d91f8d9f538c02322d
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-03-28 14:58:34 +0100

    add workaround for WebClient result properties not being defined

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { WebAPICallResult } from "@slack/web-api";
+
 // Result is a discriminated union type, taking two constrained generics and made up of intersection types for Failure and Success
 // Also uses a default parameter incase you don't want to add extra bells and whistles to your response
 // Going against convention and using Success on the Left side to make Errors optional
@@ -19,3 +21,12 @@ export type Answer = {
   userId: string;
   answer: string;
 };
+
+// unfortunate workaround - see https://slack.dev/node-slack-sdk/typescript
+export interface SlackChatPostMessageResult extends WebAPICallResult {
+  channel: string;
+  ts: string;
+  message: {
+    text: string;
+  };
+}
