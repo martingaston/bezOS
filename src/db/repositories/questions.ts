0f73474d288899fef8b5a7bf5ed3aebb34383d57
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-04-03 15:37:29 +0100

    add methods to questions repository

--- src/db/repositories/questions.ts
+++ src/db/repositories/questions.ts
@@ -2,13 +2,23 @@ import { IDatabase, IMain } from "pg-promise";
 
 export type QuestionType = "MULTIPLE_CHOICE" | "MULTIPLE_RESPONSE";
 
-export type Question = {
+type Question = {
   text: string;
   type: QuestionType;
   options: QuestionOption[];
   answer: QuestionAnswer;
   source: string;
 };
+
+type intId = {
+  id: number;
+};
+
+type uuidId = {
+  id: string;
+};
+
+export type InsertedQuestion = Question & intId;
 
 type QuestionOption = {
   name: string;
@@ -25,10 +35,26 @@ type Source = {
   name: string;
 };
 
+type Round = {
+  id: number;
+  name: string;
+  description: string;
+};
+
+type RoundQuestion = {
+  questionId: number;
+  roundId: number;
+  startDate: Date;
+  endDate: Date;
+  active: boolean;
+};
+
+type InsertedRoundQuestion = RoundQuestion & uuidId;
+
 export class QuestionsRepository {
   constructor(private db: IDatabase<unknown>, private pgp: IMain) {}
 
-  async addNewQuestion(question: Question): Promise<Question> {
+  async addNewQuestion(question: Question): Promise<InsertedQuestion> {
     return this.db.one(
       "INSERT INTO bezos.questions (text, type, options, answer, source) VALUES (${text}, ${type}, ${options:json}, ${answer}, ${source}) RETURNING *;",
       question
@@ -39,6 +65,22 @@ export class QuestionsRepository {
     return this.db.one(
       "INSERT INTO bezos.sources (name) VALUES ($1) ON CONFLICT (name) DO UPDATE SET name = bezos.sources.name RETURNING *",
       name
+    );
+  }
+
+  async addRound(name: string, description: string): Promise<Round> {
+    return this.db.one(
+      "INSERT INTO bezos.rounds (name, description) VALUES ($1, $2) RETURNING *",
+      [name, description]
+    );
+  }
+
+  async scheduleRoundQuestion(
+    roundQuestion: RoundQuestion
+  ): Promise<InsertedRoundQuestion> {
+    return this.db.one(
+      "INSERT INTO bezos.rounds_questions (question_id, round_id, active) VALUES (${questionId}, ${roundId}, FALSE) RETURNING *",
+      roundQuestion
     );
   }
 }
