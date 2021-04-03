ba2ffb074cf9c975e601e58e60fff163252ea1e3
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-04-03 15:37:03 +0100

    add auto camelCasing to sql responses

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,23 @@
-import pgPromise, { IDatabase } from "pg-promise";
+import pgPromise, { IDatabase, ITask } from "pg-promise";
 import { getEnv } from "../util/getEnv";
 import { DbExtensions, QuestionsRepository } from "./repositories";
+import camelcaseKeys from "camelcase-keys";
 
-type ExtendedProtocol = IDatabase<DbExtensions> & DbExtensions;
+type Db = IDatabase<DbExtensions> & DbExtensions;
+type Tx = ITask<DbExtensions> & DbExtensions;
 
 const initOptions = {
-  extend(obj: ExtendedProtocol) {
+  extend(obj: Db) {
     obj.questions = new QuestionsRepository(obj, pgp);
+  },
+  // TODO type this properly
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  receive(data: any, result: any) {
+    result.rows = camelcaseKeys(data);
   },
 };
 
 const pgp = pgPromise(initOptions);
 const db = pgp(getEnv("PG_CONNECTION_STRING"));
 
-export { pgp, db };
+export { pgp, db, Tx };
